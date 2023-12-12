1d336944c8941a0ab610202d496abe0bc7d48868
Luis Lopez <luis.dev.master@gmail.com>
2023-12-12 15:07:22 -0600

    Creating Index method in ProductsController

--- Presentation/Controllers/ProductsController.cs
+++ Presentation/Controllers/ProductsController.cs
@@ -1,6 +1,11 @@
-﻿namespace Presentation.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
 
 public class ProductsController : BaseController
 {
-    
+    public IActionResult Index()
+    {
+        return View();
+    }
 }
