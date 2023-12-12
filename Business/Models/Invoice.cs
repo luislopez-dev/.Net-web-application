cd2fe4b88787b9e50eea60764448eed905813ec5
Luis Lopez <luis.dev.master@gmail.com>
2023-12-12 16:30:38 -0600

    Fixing Invoice model

--- Business/Models/Invoice.cs
+++ Business/Models/Invoice.cs
@@ -5,8 +5,8 @@ public class Invoice
     public int Id { get; set; }
     public string ClientName { get; set; }
     public string ClientAddress { get; set; }
-    public string ClientNIT { get; set; }
+    public string ClientNit { get; set; }
     public float Discount { get; set; }
     public float Total { get; set; }
-    public string PaymetnMethod { get; set; }
+    public string PaymentMethod { get; set; }
 }
