0594d9e247b81a3bf8fd751f53e7ebac74f9d4be
Luis Lopez <luis.dev.master@gmail.com>
2023-12-12 16:25:02 -0600

    Creating properties for Product model

--- Business/Models/Product.cs
+++ Business/Models/Product.cs
@@ -2,5 +2,9 @@
 
 public class Product
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float Price { get; set; }
+    public int Stock { get; set; }
+    public string Description { get; set; }
 }
