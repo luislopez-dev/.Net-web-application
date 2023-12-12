64ecd20db5979d0f12f9c6dadf4639a71817ce03
Luis Lopez <luis.dev.master@gmail.com>
2023-12-12 16:24:41 -0600

    Creating properties for Invoice model

--- Business/Models/Invoice.cs
+++ Business/Models/Invoice.cs
@@ -2,5 +2,11 @@
 
 public class Invoice
 {
-    
+    public int Id { get; set; }
+    public string ClientName { get; set; }
+    public string ClientAddress { get; set; }
+    public string ClientNIT { get; set; }
+    public float Discount { get; set; }
+    public float Total { get; set; }
+    public string PaymetnMethod { get; set; }
 }
