c3f999379c10611bfa0d664ba9e82207e2fc1161
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 15:26:09 -0600

    Adjusting GetProduct in Business/Interfaces/IProductRepository.cs

--- Business/Interfaces/IProductRepository.cs
+++ Business/Interfaces/IProductRepository.cs
@@ -6,7 +6,7 @@ public interface IProductRepository
 {
     public void AddProduct(Product product);
     public void DeleteProduct(Product product);
-    public void UpdateInvoice(Product product);
+    public void UpdateProduct(Product product);
     public Task<List<Product>> GetProducts();
-    public Task<Product> GetProduct();
+    public Task<Product> GetProduct(int id);
 }
