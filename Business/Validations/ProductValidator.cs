06432daa733a556b9750678eef882e804aa0d9f7
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 14:33:04 -0600

    Adding validations for Presentation/Controllers/ProductsController.cs

--- Business/Validations/ProductValidator.cs
+++ Business/Validations/ProductValidator.cs
@@ -1,4 +1,5 @@
-﻿using Business.Models;
+﻿using System.Data;
+using Business.Models;
 using FluentValidation;
 
 namespace Business.Validations;
@@ -7,5 +8,20 @@ public class ProductValidator: AbstractValidator<Product>
 {
     public ProductValidator()
     {
+        RuleFor(product => product.Name)
+            .NotNull()
+            .WithName("¡El nombre del producto no debe estar vacio!");
+        
+        RuleFor(product => product.Price)
+            .NotNull()
+            .WithName("¡El precio del producto no debe estar vacio!");
+        
+        RuleFor(product => product.Description)
+            .NotNull()
+            .WithName("¡La descripción del producto!");
+        
+        RuleFor(product => product.Stock)
+            .NotNull()
+            .WithName("¡El stock del producto no debe estar vacio!");
     }
 }
