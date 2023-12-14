453a6ed41b06c7509105520a067efd89be25ff77
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 15:25:35 -0600

    Removing unncesary methods in Business/Interfaces/IInvoiceRepository.cs

--- Business/Interfaces/IInvoiceRepository.cs
+++ Business/Interfaces/IInvoiceRepository.cs
@@ -5,8 +5,5 @@ namespace Business.Interfaces;
 public interface IInvoiceRepository
 {
     public void AddInvoice(Invoice invoice);
-    public void DeleteInvoice(Invoice invoice);
-    public void UpdateInvoice(Invoice invoice);
     public Task<List<Invoice>> GetInvoices();
-    public Task<Invoice> GetInvoice();
 }
