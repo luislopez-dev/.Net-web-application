5d06cc143f3237a2b29a570518e04f672cfa18b0
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 06:19:49 -0600

    Implementing methods in Business/Interfaces/IInvoiceRepository.cs

--- Business/Interfaces/IInvoiceRepository.cs
+++ Business/Interfaces/IInvoiceRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Business.Interfaces;
+﻿using Business.Models;
+
+namespace Business.Interfaces;
 
 public interface IInvoiceRepository
 {
-
+    public void AddInvoice(Invoice invoice);
+    public void DeleteInvoice(Invoice invoice);
+    public void UpdateInvoice(Invoice invoice);
+    public Task<List<Invoice>> GetInvoices();
+    public Task<Invoice> GetInvoice();
 }
