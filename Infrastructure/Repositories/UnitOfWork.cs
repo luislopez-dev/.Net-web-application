46d829220e3a8bff7e991b1387a67b722177c694
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 04:40:00 -0600

    Implementing interface in UnitOfWork.cs

--- Infrastructure/Repositories/UnitOfWork.cs
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -1,6 +1,24 @@
-﻿namespace Infrastructure.Repositories;
+﻿using Business.Interfaces;
+using Infrastructure.Data;
 
-public class UnitOfWork
+namespace Infrastructure.Repositories;
+
+public class UnitOfWork: IUnitOfWork
 {
-    
+    private readonly DataContext _context;
+
+    public UnitOfWork(DataContext context)
+    {
+        _context = context;
+    }
+    public IProductRepository ProductRepository => new ProductRepository(_context);
+    public IInvoiceRepository InvoiceRepository => new InvoiceRepository(_context);
+    public Task<bool> Complete()
+    {
+        throw new NotImplementedException();
+    }
+    public bool HasChanges()
+    {
+        throw new NotImplementedException();
+    }
 }
