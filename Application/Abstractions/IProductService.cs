7d30973cb7ae8c38988fea44461b4084758d2f74
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 16:02:07 -0600

    Renaming method in Application/Abstractions/IProductService.cs

--- Application/Abstractions/IProductService.cs
+++ Application/Abstractions/IProductService.cs
@@ -7,6 +7,6 @@ public interface IProductService
     public void AddProduct(Product product);
     public void DeleteProduct(Product product);
     public void UpdateInvoice(Product product);
-    public Task<List<Product>> GetProducts();
-    public Task<Product> GetProduct(int id);
+    public Task<List<Product>> GetProductsAsync();
+    public Task<Product> GetProductAsync(int id);
 }
