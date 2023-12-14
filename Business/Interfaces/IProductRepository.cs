cf36a2415ef78514f89cc4915d5cf9c63cbfdc3f
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 15:52:10 -0600

    Renaming method

--- Business/Interfaces/IProductRepository.cs
+++ Business/Interfaces/IProductRepository.cs
@@ -7,6 +7,6 @@ public interface IProductRepository
     public void AddProduct(Product product);
     public void DeleteProduct(Product product);
     public void UpdateProduct(Product product);
-    public Task<List<Product>> GetProducts();
-    public Task<Product> GetProduct(int id);
+    public Task<List<Product>> GetProductsAsync();
+    public Task<Product> GetProductAsync(int id);
 }
