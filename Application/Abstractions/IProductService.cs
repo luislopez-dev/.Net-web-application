db73c0ffe276f1b98bbd370ea1994f3798c20c32
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 06:17:13 -0600

    Implementing methods in IProductService.cs

--- Application/Abstractions/IProductService.cs
+++ Application/Abstractions/IProductService.cs
@@ -1,6 +1,12 @@
-﻿namespace Business.Interfaces;
+﻿using Business.Models;
+
+namespace Application.Abstractions;
 
 public interface IProductService
 {
-    
+    public void AddProduct(Product product);
+    public void DeleteProduct(Product product);
+    public void UpdateInvoice(Product product);
+    public Task<List<Product>> GetProducts();
+    public Task<Product> GetProduct();
 }
