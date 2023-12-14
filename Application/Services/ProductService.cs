824cb4b694b1c86e718ea9ff311bb84358597670
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 06:19:25 -0600

    Implementing methods in Application/Services/ProductService.cs

--- Application/Services/ProductService.cs
+++ Application/Services/ProductService.cs
@@ -1,6 +1,32 @@
-﻿namespace Application.Services;
+﻿using Application.Abstractions;
+using Business.Models;
 
-public class ProductService
+namespace Application.Services;
+
+public class ProductService: IProductService
 {
-    
+    public void AddProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateInvoice(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Product>> GetProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetProduct()
+    {
+        throw new NotImplementedException();
+    }
 }
