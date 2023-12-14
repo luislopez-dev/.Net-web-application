fbb3619105929aa9753e00f77e771c4b1dd1f60b
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 15:51:49 -0600

    Renaming method in Business/Interfaces/IInvoiceRepository.cs

--- Business/Interfaces/IInvoiceRepository.cs
+++ Business/Interfaces/IInvoiceRepository.cs
@@ -5,5 +5,5 @@ namespace Business.Interfaces;
 public interface IInvoiceRepository
 {
     public void AddInvoice(Invoice invoice);
-    public Task<List<Invoice>> GetInvoices();
+    public Task<List<Invoice>> GetInvoicesAsync();
 }
