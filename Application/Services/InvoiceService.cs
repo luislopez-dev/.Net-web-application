2ff3c36db8e71cb665f00b8a32a30614cc850e71
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 06:18:57 -0600

    Implementing methods in InvoiceService.cs

--- Application/Services/InvoiceService.cs
+++ Application/Services/InvoiceService.cs
@@ -1,6 +1,32 @@
-﻿namespace Application.Services;
+﻿using Application.Abstractions;
+using Business.Models;
 
-public class InvoiceService
+namespace Application.Services;
+
+public class InvoiceService: IInvoiceService
 {
-    
+    public void AddInvoice(Invoice invoice)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteInvoice(Invoice invoice)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateInvoice(Invoice invoice)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task GetInvoices()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Invoice> GetInvoice()
+    {
+        throw new NotImplementedException();
+    }
 }
