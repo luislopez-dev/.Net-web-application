ce7c2e87f7254d30c208f46d5f1b4416790a2e72
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 06:16:50 -0600

    Implementing methods in IInvoiceService.cs

--- Application/Abstractions/IInvoiceService.cs
+++ Application/Abstractions/IInvoiceService.cs
@@ -1,6 +1,12 @@
-﻿namespace Business.Interfaces;
+﻿using Business.Models;
+
+namespace Application.Abstractions;
 
 public interface IInvoiceService
 {
-    
+    public void AddInvoice(Invoice invoice);
+    public void DeleteInvoice(Invoice invoice);
+    public void UpdateInvoice(Invoice invoice);
+    public Task GetInvoices();
+    public Task<Invoice> GetInvoice();
 }
