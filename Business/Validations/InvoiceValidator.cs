17a355e76c59c6c1559789ca8cd1b3ea3fd7ddbb
Luis Lopez <luis.dev.master@gmail.com>
2023-12-14 14:32:40 -0600

    Adding validations for Business/Validations/InvoiceValidator.cs

--- Business/Validations/InvoiceValidator.cs
+++ Business/Validations/InvoiceValidator.cs
@@ -1,9 +1,35 @@
-﻿using Business.Models;
+﻿using System.Data;
+using Business.Models;
 using FluentValidation;
 
 namespace Business.Validations;
 
 public class InvoiceValidator: AbstractValidator<Invoice>
 {
-    
+    public InvoiceValidator()
+    {
+        RuleFor(invoice => invoice.ClientName)
+            .NotNull()
+        .WithName("¡El nombre del cliente no debe estar vacio!");
+        
+        RuleFor(invoice => invoice.ClientAddress)
+            .NotNull()
+            .WithName("¡La dirección del cliente no debe estar vacia!");
+        
+        RuleFor(invoice => invoice.ClientNit)
+            .NotNull()
+            .WithName("¡El NIT del cliente no debe estar vacio!");
+        
+        RuleFor(invoice => invoice.Discount)
+            .NotNull()
+            .WithName("¡El descuento en la factura no debe estar vacio!");
+
+        RuleFor(invoice => invoice.Total)
+            .NotNull()
+            .WithName("¡El total de la factura no debe estar vacio!");
+
+        RuleFor(invoice => invoice.PaymentMethod)
+            .NotNull()
+            .WithName("!El método de pago no debe estar vacio¡");
+    }
 }
