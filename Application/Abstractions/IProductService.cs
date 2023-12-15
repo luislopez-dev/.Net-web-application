7b124abd8f673c494cb2b1a91c17d6d24c69db5d
Luis Lopez <luis.dev.master@gmail.com>
2023-12-15 11:37:27 -0600

    Fixing method name in Application/Abstractions/IProductService.cs

--- Application/Abstractions/IProductService.cs
+++ Application/Abstractions/IProductService.cs
@@ -6,7 +6,7 @@ public interface IProductService
 {
     public void AddProduct(Product product);
     public void DeleteProduct(Product product);
-    public void UpdateInvoice(Product product);
+    public void UpdateProduct(Product product);
     public Task<List<Product>> GetProductsAsync();
     public Task<Product> GetProductAsync(int id);
 }
