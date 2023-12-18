a704a009f835942fc30f127767e77994b548e561
Luis Lopez <luis.dev.master@gmail.com>
2023-12-18 01:22:33 -0600

    Implementing renamed method in Business/Interfaces/IInvoiceRepository.cs

--- Business/Interfaces/IInvoiceRepository.cs
+++ Business/Interfaces/IInvoiceRepository.cs
@@ -5,5 +5,5 @@ namespace Business.Interfaces;
 public interface IInvoiceRepository
 {
     public void AddInvoice(Invoice invoice);
-    public Task<List<Invoice>> GetInvoicesAsync();
+    public Task<List<Invoice>> GetInvoicesPaginatedAsync();
 }
