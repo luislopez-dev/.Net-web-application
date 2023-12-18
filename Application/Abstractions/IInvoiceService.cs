2f2776bd0a21b563f61c3e51bd0cb304d10107df
Luis Lopez <luis.dev.master@gmail.com>
2023-12-18 01:21:53 -0600

    Renaming method in Application/Abstractions/IInvoiceService.cs

--- Application/Abstractions/IInvoiceService.cs
+++ Application/Abstractions/IInvoiceService.cs
@@ -5,5 +5,5 @@ namespace Application.Abstractions;
 public interface IInvoiceService
 {
     public void AddInvoice(Invoice invoice);
-    public Task<List<Invoice>> GetInvoicesAsync();
+    public Task<List<Invoice>> GetInvoicesPaginatedAsync();
 }
