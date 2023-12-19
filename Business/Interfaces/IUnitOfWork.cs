ea79ae0930e1bfed5a7cf87002d0a225cb753e2d
Luis Lopez <luis.dev.master@gmail.com>
2023-12-19 15:27:43 -0600

    Replacing Order interface by InvoiceProduct Interface

--- Business/Interfaces/IUnitOfWork.cs
+++ Business/Interfaces/IUnitOfWork.cs
@@ -4,6 +4,7 @@ public interface IUnitOfWork
 {
     IProductRepository ProductRepository { get; }
     IInvoiceRepository InvoiceRepository { get; }
+    IInvoiceProductRepository InvoiceProductRepository { get; }
     Task<bool> CompleteAsync();
     bool HasChanges();
 }
