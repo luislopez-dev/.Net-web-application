2f12c1c62f5e85fe8cfc70b31d93d1351653af51
Luis Lopez <luis.dev.master@gmail.com>
2023-12-18 23:10:28 -0600

    Infrastructure/Repositories/OrderRepository.cs

---
+++ Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Data;
+
+namespace Infrastructure.Repositories;
+
+public class OrderRepository
+{
+    private readonly DataContext _context;
+
+    public OrderRepository(DataContext context)
+    {
+        _context = context;
+    }
+    
+}
