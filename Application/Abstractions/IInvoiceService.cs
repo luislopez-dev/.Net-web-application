a9c2c1fd299f8a0758f38f0d365acbfb00f2d274
Luis Lopez <luis.dev.master@gmail.com>
2023-12-19 15:55:32 -0600

    Adding selectedProducts parameter in createRecord method in Application/Abstractions/IInvoiceService.cs

--- Application/Abstractions/IInvoiceService.cs
+++ Application/Abstractions/IInvoiceService.cs
@@ -4,6 +4,6 @@ namespace Application.Abstractions;
 
 public interface IInvoiceService
 {
-    public void AddInvoice(Invoice invoice);
+    public void AddInvoice(Invoice invoice, int[] selectedProducts);
     public Task<List<Invoice>> GetInvoicesPaginatedAsync();
 }
