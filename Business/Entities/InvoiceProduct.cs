4f896237491b9516c586c2266616b6cfdc857bdd
Luis Lopez <luis.dev.master@gmail.com>
2023-12-19 15:38:45 -0600

    Creating validations in InvoiceProduct Entity

--- Business/Entities/InvoiceProduct.cs
+++ Business/Entities/InvoiceProduct.cs
@@ -1,12 +1,16 @@
-﻿namespace Business.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Business.Models;
 
 public class InvoiceProduct
 {
     public int Id { get; set; }
-
+    
+    [Required]
     public int ProductId { get; set; }
     public Product Product { get; set; }
 
+    [Required]
     public int InvoiceId { get; set; }
     public Invoice Invoice { get; set; }
 }
