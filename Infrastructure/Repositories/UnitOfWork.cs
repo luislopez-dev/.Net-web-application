ef8158c48c76a35a25c41830c10a0181ab87cab1
Luis Lopez <luis.dev.master@gmail.com>
2023-12-19 15:30:51 -0600

    Replacing Order by InvoiceProduct in Infrastructure/Repositories/UnitOfWork.cs

--- Infrastructure/Repositories/UnitOfWork.cs
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -13,6 +13,8 @@ public class UnitOfWork: IUnitOfWork
     }
     public IProductRepository ProductRepository => new ProductRepository(_context);
     public IInvoiceRepository InvoiceRepository => new InvoiceRepository(_context);
+    public IInvoiceProductRepository InvoiceProductRepository => new InvoiceProductRepository(_context);
+
     public async Task<bool> CompleteAsync()
     {
         return await _context.SaveChangesAsync() > 0;
