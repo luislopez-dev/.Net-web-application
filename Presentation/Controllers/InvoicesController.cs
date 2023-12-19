e423c742dbae6a3566712904b644a975a6c481b6
Luis Lopez <luis.dev.master@gmail.com>
2023-12-19 12:44:18 -0600

    Removing service manager instances in Presentation/Controllers/InvoicesController.cs

--- Presentation/Controllers/InvoicesController.cs
+++ Presentation/Controllers/InvoicesController.cs
@@ -7,20 +7,17 @@ namespace Presentation.Controllers;
 
 public class InvoicesController : BaseController
 {
-    private readonly IServiceManager _serviceManager;
-    
     private readonly IUnitOfWork _unitOfWork;
 
-    public InvoicesController(IServiceManager serviceManager, IUnitOfWork unitOfWork)
+    public InvoicesController(IUnitOfWork unitOfWork)
     {
-        _serviceManager = serviceManager;
         _unitOfWork = unitOfWork;
     }
 
     public async Task<IActionResult> Index()
     {
-        var invoices = await _serviceManager
-            .InvoiceService
+        var invoices = await _unitOfWork
+            .InvoiceRepository
             .GetInvoicesPaginatedAsync();
         
         return View(invoices);
@@ -28,8 +25,8 @@ public class InvoicesController : BaseController
     
     public async Task<IActionResult> Create()
     {
-        ViewBag.products = await _serviceManager
-            .ProductService
+        ViewBag.products = await _unitOfWork
+            .ProductRepository
             .GetProductsPaginatedAsync();
         
         return View();
@@ -44,8 +41,6 @@ public class InvoicesController : BaseController
             TempData["message"] = "Campos invalidos!";
             return View(nameof(Index));
         }
-        
-        _serviceManager.InvoiceService.AddInvoice(invoice, selectedProducts);
         
         if(await _unitOfWork.CompleteAsync()){
             TempData["message"] = "Factura creada exitosamente!";
