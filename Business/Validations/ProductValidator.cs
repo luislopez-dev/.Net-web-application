93badafa0db672168eec8c6239f17dc5e516304e
Luis Lopez <luis.dev.master@gmail.com>
2023-12-21 10:37:22 -0600

    Creating comments in Business/Validations/ProductValidator.cs

--- Business/Validations/ProductValidator.cs
+++ Business/Validations/ProductValidator.cs
@@ -1,25 +1,28 @@
-﻿using System.Data;
-using Business.Models;
+﻿using Business.Models;
 using FluentValidation;
 
 namespace Business.Validations;
 
-public class ProductValidator: AbstractValidator<Product>
-{
+public class ProductValidator: AbstractValidator<Product> {
+
     public ProductValidator()
     {
+        // Validations for Name attribute
         RuleFor(product => product.Name)
             .NotNull()
             .WithName("¡El nombre del producto no debe estar vacio!");
         
+        // Validations for Price attribute
         RuleFor(product => product.Price)
             .NotNull()
             .WithName("¡El precio del producto no debe estar vacio!");
         
+        // Validations for Description attribute
         RuleFor(product => product.Description)
             .NotNull()
             .WithName("¡La descripción del producto!");
-        
+            
+        // Validations for Stock attribute
         RuleFor(product => product.Stock)
             .NotNull()
             .WithName("¡El stock del producto no debe estar vacio!");
