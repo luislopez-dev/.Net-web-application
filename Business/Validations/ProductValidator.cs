d9aa23309f73b1fbedf2da09b8947ebbf59781f9
Luis Lopez <luis.dev.master@gmail.com>
2023-12-21 21:38:29 -0600

    Adding validations to Product model attributes

--- Business/Validations/ProductValidator.cs
+++ Business/Validations/ProductValidator.cs
@@ -12,24 +12,27 @@ public class ProductValidator: AbstractValidator<Product> {
             .NotNull()
             .WithName("¡El nombre del producto no debe estar vacio!");
             .Length(5, 60)
+            .WithName("El nombre del prudcot debe tener entre 5 a 60 caracteres");
         
         // Validations for Price attribute
         RuleFor(product => product.Price)
             .NotNull()
             .WithName("¡El precio del producto no debe estar vacio!");
             .InclusiveBetween(0.10, 100000)
+            .WithName("El precio del producto debe ser entre 10 centavos a 100,000 quetzales")
         
         // Validations for Description attribute
         RuleFor(product => product.Description)
             .NotNull()
             .WithName("¡La descripción del producto no debe estar vacia!");
             .Length(5, 900)
-            
+            .WithName("La descripción del producto debe tener entre 5 a 900 caracteres")            
             
         // Validations for Stock attribute
         RuleFor(product => product.Stock)
             .NotNull()
             .WithName("¡El stock del producto no debe estar vacio!");
             .InclusiveBetween(1, 1000000)
+            .WithName("El stock del producto debe estar entre una a un millón de unidades")
     }
 }
