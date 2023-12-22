840efb52ed01e6446010fad28a08b95a739dc1d9
Luis Lopez <luis.dev.master@gmail.com>
2023-12-21 22:44:38 -0600

    Creating validations for Invoice model

--- Business/Validations/InvoiceValidator.cs
+++ Business/Validations/InvoiceValidator.cs
@@ -10,26 +10,38 @@ public class InvoiceValidator: AbstractValidator<Invoice>
     {
         RuleFor(invoice => invoice.ClientName)
             .NotNull()
-        .WithName("¡El nombre del cliente no debe estar vacio!");
+            .WithName("¡El nombre del cliente no debe estar vacio!")
+            .Length(5, 70)
+            .WithName("El nombre del cliente debe tener entre 5 a 70 caracteres");
         
         RuleFor(invoice => invoice.ClientAddress)
             .NotNull()
-            .WithName("¡La dirección del cliente no debe estar vacia!");
+            .WithName("¡La dirección del cliente no debe estar vacia!")
+            .Length(20, 400)
+            .WithName("El nombre del cliente debe tener entre 5 a 400 caracteres");
         
         RuleFor(invoice => invoice.ClientNit)
             .NotNull()
-            .WithName("¡El NIT del cliente no debe estar vacio!");
+            .WithName("¡El NIT del cliente no debe estar vacio!")
+            .TotalLength(8)
+            .WithName("El NIT debe tener 8 caracteres");
         
         RuleFor(invoice => invoice.Discount)
             .NotNull()
             .WithName("¡El descuento en la factura no debe estar vacio!");
+            .InclusiveBetween(0, 1000000)
+            .WithName("¡La cifra del descuento en la factura debe estar entre 0 y un millón!");
 
         RuleFor(invoice => invoice.Total)
             .NotNull()
-            .WithName("¡El total de la factura no debe estar vacio!");
+            .WithName("¡El total de la factura no debe estar vacio!")
+            .InclusiveBetween(0, 1000000)
+            .WithName("¡La cifra del total en la factura debe estar entre 0 y un millón!");
 
         RuleFor(invoice => invoice.PaymentMethod)
             .NotNull()
-            .WithName("!El método de pago no debe estar vacio¡");
+            .WithName("!El método de pago no debe estar vacio¡")
+            .Must(paymentMethod => new[] {"Debit card", "Credit Card", "Cash"}.Contains(paymentMethod))
+            .WithName("¡El método de pago ingresado no es válido!");
     }
 }
