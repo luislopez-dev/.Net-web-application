95b4c950eccc4a1cb78a732947fa00c3d862e7c2
Luis Lopez <luis.dev.master@gmail.com>
2023-12-21 21:34:50 -0600

    Fixing NotNull validation order for Product model attribures

--- Business/Validations/ProductValidator.cs
+++ Business/Validations/ProductValidator.cs
@@ -10,25 +10,26 @@ public class ProductValidator: AbstractValidator<Product> {
         // Validations for Name attribute
         RuleFor(product => product.Name)
             .NotNull()
-            .Length(5, 60)
             .WithName("¡El nombre del producto no debe estar vacio!");
+            .Length(5, 60)
         
         // Validations for Price attribute
         RuleFor(product => product.Price)
             .NotNull()
-            .InclusiveBetween(0.10, 100000)
             .WithName("¡El precio del producto no debe estar vacio!");
+            .InclusiveBetween(0.10, 100000)
         
         // Validations for Description attribute
         RuleFor(product => product.Description)
             .NotNull()
+            .WithName("¡La descripción del producto no debe estar vacia!");
             .Length(5, 900)
-            .WithName("¡La descripción del producto!");
+            
             
         // Validations for Stock attribute
         RuleFor(product => product.Stock)
             .NotNull()
-            .InclusiveBetween(1, 1000000)
             .WithName("¡El stock del producto no debe estar vacio!");
+            .InclusiveBetween(1, 1000000)
     }
 }
