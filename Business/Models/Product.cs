b508001f9e19e11418c2bf039d39384bb681ebd4
Luis Lopez <luis.dev.master@gmail.com>
2023-12-21 23:04:45 -0600

    Creating uuid field in Business/Models/Product.cs

--- Business/Models/Product.cs
+++ Business/Models/Product.cs
@@ -3,6 +3,7 @@
 public class Product
 {
     public int Id { get; set; }
+    public Guid Uuid { get; set; }
     public string Name { get; set; }
     public float Price { get; set; }
     public int Stock { get; set; }
