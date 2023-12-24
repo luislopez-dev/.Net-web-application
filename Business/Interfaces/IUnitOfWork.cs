295a1af14b9814ed434da499cb950321b1e2300a
Luis Lopez <luis.dev.master@gmail.com>
2023-12-24 16:54:00 -0600

    Creating BeginTransaction method in Business/Interfaces/IUnitOfWork.cs

--- Business/Interfaces/IUnitOfWork.cs
+++ Business/Interfaces/IUnitOfWork.cs
@@ -1,10 +1,19 @@
-﻿namespace Business.Interfaces;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Business.Interfaces;
 
 public interface IUnitOfWork
 {
     IProductRepository ProductRepository { get; }
+    
     IInvoiceRepository InvoiceRepository { get; }
+    
     IInvoiceProductRepository InvoiceProductRepository { get; }
+    
     Task<bool> CompleteAsync();
+    
     bool HasChanges();
+    
+    IDbContextTransaction BeginTransaction(CancellationToken cancellationToken);
+    
 }
