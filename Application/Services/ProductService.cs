4e5f6b7b98ab25f7abb79f77032f5eb65712c9b4
Luis Lopez <luis.dev.master@gmail.com>
2023-12-24 18:42:34 -0600

    Adding support for cancellation token in Application/Services/ProductService.cs

--- Application/Services/ProductService.cs
+++ Application/Services/ProductService.cs
@@ -7,50 +7,65 @@ namespace Application.Services;
 public class ProductService: IProductService
 {
     private readonly IUnitOfWork _unitOfWork;
+    
     public ProductService(IUnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
     }
-    public async Task AddProductAsync(Product product)
+    public async Task AddProductAsync(Product product,CancellationToken cancellationToken)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+        
         await _unitOfWork
             .ProductRepository
-            .AddProductAsync(product);
+            .AddProductAsync(product, cancellationToken);
     }
-    public async Task DeleteProductByGuidAsync(Guid guid)
+    public async Task DeleteProductByGuidAsync(Guid guid, CancellationToken cancellationToken)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         await _unitOfWork
             .ProductRepository
-            .DeleteProductByGuidAsync(guid);
+            .DeleteProductByGuidAsync(guid, cancellationToken);
     }
-    public void UpdateProduct(Product product)
+    public void UpdateProduct(Product product, CancellationToken cancellationToken)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         _unitOfWork
             .ProductRepository
-            .UpdateProduct(product);
+            .UpdateProduct(product, cancellationToken);
     }
-    public async Task<List<Product>> GetProductsPaginatedAsync()
+    public async Task<List<Product>> GetProductsPaginatedAsync(CancellationToken cancellationToken)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         return await _unitOfWork
             .ProductRepository
-            .GetProductsPaginatedAsync();
+            .GetProductsPaginatedAsync(cancellationToken);
     }
-    public async Task<Product> GetProductByIdAsync(int id)
+    public async Task<Product> GetProductByIdAsync(int id, CancellationToken cancellationToken)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         return await _unitOfWork
             .ProductRepository
-            .GetProductByIdAsync(id);
+            .GetProductByIdAsync(id, cancellationToken);
     }
-    public async Task<Product> GetProductByGuidAsync(Guid guid)
+    public async Task<Product> GetProductByGuidAsync(Guid guid,CancellationToken cancellationToken)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         return await _unitOfWork
             .ProductRepository
-            .GetProductByGuidAsync(guid);
+            .GetProductByGuidAsync(guid, cancellationToken);
     }
-    public async Task<List<Product>> GetProductsByNamePaginatedAsync(string name)
+    public async Task<List<Product>> GetProductsByNamePaginatedAsync(string name, CancellationToken cancellationToken)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         return await _unitOfWork
             .ProductRepository
-            .GetProductsByNamePaginated(name);
+            .GetProductsByNamePaginated(name, cancellationToken);
     }
 }
