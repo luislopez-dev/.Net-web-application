f5b0bcc20e4f4d005d53a1309b6b99e4066e3f56
Luis Lopez <luis.dev.master@gmail.com>
2023-12-24 18:44:14 -0600

    Adding suppor for cancellation token in Infrastructure/Repositories/ProductRepository.cs

--- Infrastructure/Repositories/ProductRepository.cs
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -13,50 +13,54 @@ internal class ProductRepository: IProductRepository
     {
         _context = context;
     }
-    public async Task AddProductAsync(Product product)
+    public async Task AddProductAsync(Product product, CancellationToken cancellationToken)
     {
-        await _context.AddAsync(product);
+        cancellationToken.ThrowIfCancellationRequested();
+        
+        await _context.AddAsync(product, cancellationToken);
     }
-    public async Task DeleteProductByGuidAsync(Guid guid)
+    public async Task DeleteProductByGuidAsync(Guid guid, CancellationToken cancellationToken)
     {
-        /* In case optimization is needed, use this:
-         var sql = $"DELETE FROM Products WHERE Guid = {guid}";
-         await _context.Database.ExecuteSqlRawAsync(sql);
-        */
+        cancellationToken.ThrowIfCancellationRequested();
+        
         var product = await _context.Products
-            .FirstOrDefaultAsync(m => m.Guid == guid);
+            .FirstOrDefaultAsync(m => m.Guid == guid, cancellationToken);
         _context.Remove(product);
     }
-    public void UpdateProduct(Product product)
+    public void UpdateProduct(Product product, CancellationToken cancellationToken)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+        
         _context.Entry(product).State = EntityState.Modified;
     }
-    public async Task<List<Product>> GetProductsPaginatedAsync()
+    public async Task<List<Product>> GetProductsPaginatedAsync(CancellationToken cancellationToken)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+        
         var products = from n in _context.Products select n;
         
-        return await products.ToListAsync();
+        return await products.ToListAsync(cancellationToken);
     }
-    public async Task<Product> GetProductByIdAsync(int id)
+    public async Task<Product> GetProductByIdAsync(int id, CancellationToken cancellationToken)
     {
         return await _context
             .Products
-            .FirstOrDefaultAsync(m => m.Id == id);
+            .FirstOrDefaultAsync(m => m.Id == id, cancellationToken);
     }
 
-    public async Task<Product> GetProductByGuidAsync(Guid guid)
+    public async Task<Product> GetProductByGuidAsync(Guid guid, CancellationToken cancellationToken)
     {
         return await _context
             .Products
-            .FirstOrDefaultAsync(m => m.Guid == guid);
+            .FirstOrDefaultAsync(m => m.Guid == guid, cancellationToken);
     }
 
-    public async Task<List<Product>> GetProductsByNamePaginated(string name)
+    public async Task<List<Product>> GetProductsByNamePaginated(string name, CancellationToken cancellationToken)
     {
         return await _context
             .Products
             .Where(p => EF.Functions.Like(p.Name, $"%{name}%"))
             .OrderBy(p => p.Id)
-            .ToListAsync();
+            .ToListAsync(cancellationToken);
     }
 }
