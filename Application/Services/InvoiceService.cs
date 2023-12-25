691887bb702185270ca0e713e2a5125da47a0ea1
Luis Lopez <luis.dev.master@gmail.com>
2023-12-24 18:13:41 -0600

    Including cancellationToke in  Application/Services/InvoiceService.cs

--- Application/Services/InvoiceService.cs
+++ Application/Services/InvoiceService.cs
@@ -22,20 +22,20 @@ public class InvoiceService: IInvoiceService
         // Begin Transaction
         await using var transaction = _unitOfWork
             .BeginTransaction(cancellationToken);
-
+        
         try
         {
             // Create Invoice
             await _unitOfWork
                 .InvoiceRepository
                 .AddInvoice(invoice, cancellationToken);
-            await _unitOfWork.CompleteAsync();
+            await _unitOfWork.CompleteAsync(cancellationToken);
         
             // Assign products to newly created invoice
             await _unitOfWork
                 .InvoiceProductRepository
                 .CreateRecordAsync(invoice.Id, selectedProducts, cancellationToken);
-            await _unitOfWork.CompleteAsync();
+            await _unitOfWork.CompleteAsync(cancellationToken);
             
             // Commit transaction if previous operations succeed
             await transaction.CommitAsync(cancellationToken);
