385ff0d04542df66355379e441591a7deac6703f
Luis Lopez <luis.dev.master@gmail.com>
2023-12-24 18:43:26 -0600

    Adding support for cancellation token in Business/Interfaces/IProductRepository.cs

--- Business/Interfaces/IProductRepository.cs
+++ Business/Interfaces/IProductRepository.cs
@@ -4,11 +4,11 @@ namespace Business.Interfaces;
 
 public interface IProductRepository
 {
-    public Task AddProductAsync(Product product);
-    public Task DeleteProductByGuidAsync(Guid guid);
-    public void UpdateProduct(Product product);
-    public Task<List<Product>> GetProductsPaginatedAsync();
-    public Task<Product> GetProductByIdAsync(int id);
-    public Task<Product> GetProductByGuidAsync(Guid guid);
-    public Task<List<Product>> GetProductsByNamePaginated(string name);
+    public Task AddProductAsync(Product product, CancellationToken cancellationToken);
+    public Task DeleteProductByGuidAsync(Guid guid, CancellationToken cancellationToken);
+    public void UpdateProduct(Product product, CancellationToken cancellationToken);
+    public Task<List<Product>> GetProductsPaginatedAsync(CancellationToken cancellationToken);
+    public Task<Product> GetProductByIdAsync(int id, CancellationToken cancellationToken);
+    public Task<Product> GetProductByGuidAsync(Guid guid, CancellationToken cancellationToken);
+    public Task<List<Product>> GetProductsByNamePaginated(string name, CancellationToken cancellationToken);
 }
