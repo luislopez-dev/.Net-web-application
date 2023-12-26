4c31101a60e89274d11a564dbd7efb925ff8fd00
Luis Lopez <luis.dev.master@gmail.com>
2023-12-25 21:57:34 -0600

    Creating exceptio handling in Infrastructure/Repositories/ProductRepository.cs

--- Infrastructure/Repositories/ProductRepository.cs
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using Business.Interfaces;
+﻿using Business.Exceptions.Product.Exceptions.DatabaseExceptions;
+using Business.Interfaces;
 using Business.Models;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -16,51 +17,107 @@ internal class ProductRepository: IProductRepository
     public async Task AddProductAsync(Product product, CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
-        
-        await _context.AddAsync(product, cancellationToken);
+
+        try
+        {
+            await _context.AddAsync(product, cancellationToken);
+        }
+        catch (CreateProductException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
     public async Task DeleteProductByGuidAsync(Guid guid, CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
-        
-        var product = await _context.Products
-            .FirstOrDefaultAsync(m => m.Guid == guid, cancellationToken);
-        _context.Remove(product);
+
+        try
+        {
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.Guid == guid, cancellationToken);
+            _context.Remove(product);
+        }
+        catch (DeleteProductException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
     public void UpdateProduct(Product product, CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
-        
-        _context.Entry(product).State = EntityState.Modified;
+
+        try
+        {
+            _context.Entry(product).State = EntityState.Modified;
+        }
+        catch (UpdateProductException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
     public async Task<List<Product>> GetProductsPaginatedAsync(CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
+
+        try
+        {
+            var products = from n in _context.Products select n;
         
-        var products = from n in _context.Products select n;
-        
-        return await products.ToListAsync(cancellationToken);
+            return await products.ToListAsync(cancellationToken);
+        }
+        catch (GetProductsException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
     public async Task<Product> GetProductByIdAsync(int id, CancellationToken cancellationToken)
     {
-        return await _context
-            .Products
-            .FirstOrDefaultAsync(m => m.Id == id, cancellationToken);
+        try
+        {
+            return await _context
+                .Products
+                .FirstOrDefaultAsync(m => m.Id == id, cancellationToken);
+        }
+        catch (GetProductException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 
     public async Task<Product> GetProductByGuidAsync(Guid guid, CancellationToken cancellationToken)
     {
-        return await _context
-            .Products
-            .FirstOrDefaultAsync(m => m.Guid == guid, cancellationToken);
+        try
+        {
+            return await _context
+                .Products
+                .FirstOrDefaultAsync(m => m.Guid == guid, cancellationToken);
+        }
+        catch (GetProductsException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 
     public async Task<List<Product>> GetProductsByNamePaginated(string name, CancellationToken cancellationToken)
     {
-        return await _context
-            .Products
-            .Where(p => EF.Functions.Like(p.Name, $"%{name}%"))
-            .OrderBy(p => p.Id)
-            .ToListAsync(cancellationToken);
+        try
+        {
+            return await _context
+                .Products
+                .Where(p => EF.Functions.Like(p.Name, $"%{name}%"))
+                .OrderBy(p => p.Id)
+                .ToListAsync(cancellationToken);
+        }
+        catch (GetProductsException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
