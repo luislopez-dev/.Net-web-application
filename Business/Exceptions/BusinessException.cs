5f6a7bb7774ee16bc1e64d13e29ad3406085dad4
Luis Lopez <luis.dev.master@gmail.com>
2023-12-27 04:05:00 -0600

    Adding signature

--- Business/Exceptions/BusinessException.cs
+++ Business/Exceptions/BusinessException.cs
@@ -1,4 +1,10 @@
-﻿namespace Business.Exceptions;
+﻿/*
+ * Author: Luis López
+ * Website: https://github.com/luislopez-dev
+ * Description: Training Project
+ */
+
+namespace Business.Exceptions;
 
 public class BusinessException: Exception
 {
