c59f011ca1506660cf1b0ed322dbdac0335cfae2
Luis Lopez <luis.dev.master@gmail.com>
2023-12-26 19:59:30 -0600

    Fixing Business/Exceptions/ValidationException.cs

--- Business/Exceptions/ValidationException.cs
+++ Business/Exceptions/ValidationException.cs
@@ -1,6 +1,13 @@
-﻿namespace Business.Exceptions;
+﻿using FluentValidation.Results;
+
+namespace Business.Exceptions;
 
 public class ValidationException: Exception
 {
-    
+    public List<ValidationFailure> ValidationFailures { get; }
+
+    public ValidationException(List<ValidationFailure> validationFailures)
+    {
+        ValidationFailures = validationFailures;
+    }
 }
