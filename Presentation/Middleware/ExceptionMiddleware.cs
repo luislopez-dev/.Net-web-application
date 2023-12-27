fe32f13b44f7f8dd25c50f93fb1e1df0bed08016
Luis Lopez <luis.dev.master@gmail.com>
2023-12-27 02:49:32 -0600

    Adding try/catch block to Presentation/Middleware/ExceptionMiddleware.cs

--- Presentation/Middleware/ExceptionMiddleware.cs
+++ Presentation/Middleware/ExceptionMiddleware.cs
@@ -2,5 +2,26 @@
 
 public class ExceptionMiddleware
 {
-    
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+    {
+        _next = next;
+        _logger = logger;
+        _env = env;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+        }
+    }
 }
