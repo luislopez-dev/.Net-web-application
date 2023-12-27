ee4cdcb5f490adc40ba48f497be8a9a7917247ad
Luis Lopez <luis.dev.master@gmail.com>
2023-12-27 04:03:16 -0600

    Adding signature

--- Business/Exceptions/ValidationException.cs
+++ Business/Exceptions/ValidationException.cs
@@ -1,4 +1,10 @@
-﻿using FluentValidation.Results;
+﻿/*
+ * Author: Luis López
+ * Website: https://github.com/luislopez-dev
+ * Description: Training Project
+ */
+
+using FluentValidation.Results;
 
 namespace Business.Exceptions;
 
