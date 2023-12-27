3121f8d89593677dbb8cb912eddf0f7732914b5c
Luis Lopez <luis.dev.master@gmail.com>
2023-12-27 04:05:17 -0600

    Adding signature

--- Business/Exceptions/Invoice.Exceptions/GetInvoicesException.cs
+++ Business/Exceptions/Invoice.Exceptions/GetInvoicesException.cs
@@ -1,4 +1,10 @@
-﻿namespace Business.Exceptions.Invoice.Exceptions.ValidationExceptions;
+﻿/*
+ * Author: Luis López
+ * Website: https://github.com/luislopez-dev
+ * Description: Training Project
+ */
+ 
+namespace Business.Exceptions.Invoice.Exceptions.ValidationExceptions;
 
 public class GetInvoicesException: BusinessException
 {
