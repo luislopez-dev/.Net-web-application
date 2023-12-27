bca71d0e5ca03ecc7ac05ae02e11ecdf4bf4104c
Luis Lopez <luis.dev.master@gmail.com>
2023-12-26 19:59:01 -0600

    Creating constructor in Business/Exceptions/Product.Exceptions/ValidationExceptions/ProductValidationException.cs

--- Business/Exceptions/Product.Exceptions/ValidationExceptions/ProductValidationException.cs
+++ Business/Exceptions/Product.Exceptions/ValidationExceptions/ProductValidationException.cs
@@ -1,6 +1,10 @@
-﻿namespace Business.Exceptions.Product.Exceptions.ValidationExceptions;
+﻿using FluentValidation.Results;
+
+namespace Business.Exceptions.Product.Exceptions.ValidationExceptions;
 
 public class ProductValidationException: ValidationException
 {
-    
+    public ProductValidationException(List<ValidationFailure> failures) : base(failures)
+    {
+    }
 }
